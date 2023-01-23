c8ccb5cfe2756b06f8803e5dfa32d3fe14f7ff81
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-01-23 20:55:37 +0900

    Add files via upload

---
+++ main_up.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from PIL import Image
+
+# テキスト➀
+st.title('田口サイトで～す')
+st.caption('もっと内容を充実していきます。ご安心を！')
+
+# 画像
+image=Image.open('./data/000001.jpg')
+st.image(image,width=400)
+
+# 動画
+import streamlit as st
+video_file=open('.\data\douga.MTS','rb')
+video_bvtes=video_file.read()
+st.video(video_bvtes)
