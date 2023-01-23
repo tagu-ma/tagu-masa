2ae0c6613b5d2ab3a076eebc60e86015875afb35
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-01-23 22:05:26 +0900

    Update python_1.py

--- pages/python_1.py
+++ pages/python_1.py
@@ -5,6 +5,16 @@ import streamlit as st
 # さいこう
 # いつも勝ってばかり
 '''
+st.text('花火_xls')
+uploaded_files_xlsx = st.file_uploader('ファイルアップロード', type='xlsx')
+
+submit_btn_xlsx = st.button('xlsx処理実行')
+
+#ボタンが押されたら処理を実行する
+if submit_btn_xlsx:
+_df_xlsx = pd.read_excel(uploaded_files_xlsx)
+_df_xlsx
+
 code='''
 import stream as st
 
