2be314164c626fee6b5b8b584904b315c9001d14
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-01-24 22:35:23 +0900

    Add files via upload

---
+++ pages/5 音楽・動画.py
@@ -0,0 +1,35 @@
+import streamlit as st
+# 音楽曲の取込
+import numpy as np
+
+'''
+# 作曲:ＡＩと田口作成
+'''
+audio_file = open('./data1/AI曲・田口作成.mp3', 'rb')
+audio_bytes = audio_file.read()
+
+st.audio(audio_bytes, format='audio/ogg')
+
+sample_rate = 44100  # 44100 samples per second
+seconds = 2  # Note duration of 2 seconds
+frequency_la = 440  # Our played note will be 440 Hz
+# Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+t = np.linspace(0, seconds, seconds * sample_rate, False)
+# Generate a 440 Hz sine wave
+note_la = np.sin(frequency_la * t * 2 * np.pi)
+
+st.audio(note_la, sample_rate=sample_rate)
+
+# 動画
+'''
+# 電子ピアノ演奏
+'''
+import streamlit as st
+video_file=open('./data/動画.MTS','rb')
+video_bvtes=video_file.read()
+st.video(video_bvtes)
+
+
+
+
+
