47aff412bf565db78b37cfb420dc0915e3f74623
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-01-24 22:33:25 +0900

    Add files via upload

---
+++ pages/1 自由書き.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+
+'''
+# 自由スペース
+## 自分が好きな文章やファイルなどが、自由に挿入できますよ！
+'''
+'''
+# 
+'''
+
+'''
+# 田口先生、サッカーうますぎ!!
+# いつも勝ってばかり
+# すごい、ステキ～
+'''
+
+code='''
+import stream as st
+
+st.title('アプリ')
+'''
+st.code(code,language='python')
