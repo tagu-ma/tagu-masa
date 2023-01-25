11e8af452e0b1b4af0c0e7850e29768696ae4cb9
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-01-25 13:01:29 +0900

    Update 4 エクセル.py

--- pages/4 エクセル.py
+++ pages/4 エクセル.py
@@ -3,13 +3,13 @@ import pandas as pd
 # Excelファイルの読込
 
 st.title('L-gate・おすすめサイト一覧')
-df = pd.read_excel(r'E:\平原小・唐津市\R4年・パイソン・Python\⑳_2アプリ公開\data\L-gate・おすすめサイト一覧.xlsx',sheet_name= 'タイピングサイト')
+df = pd.read_excel('E:\平原小・唐津市\R4年・パイソン・Python\⑳_2アプリ公開\data\L-gate・おすすめサイト一覧.xlsx',sheet_name= 'タイピングサイト')
 st.dataframe(df)
 #st.table(df)
 
 # Excelマクロの読込
 st.title('マクロExcel・付箋作成・縦配置')
-xlsm = st.file_uploader(r'./data/マクロExcel・付箋作成・縦配置.xlsm')
+xlsm = st.file_uploader('./data/マクロExcel・付箋作成・縦配置.xlsm')
 
 btn_xlsm = st.button('xlsm処理実行')
 #ボタンが押されたら処理を実行する
@@ -19,7 +19,7 @@ if btn_xlsm:
      
  # PowerPointの読込    
 st.title('花火')
-pptx = st.file_uploader(r'./data/花火.pptx')
+pptx = st.file_uploader('./data/花火.pptx')
 
 btn_pptx = st.button('pptx処理実行')
 #ボタンが押されたら処理を実行する
