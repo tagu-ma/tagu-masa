4a023c526183f1a0f4da7361ee91141d7058f19d
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-01-31 22:06:13 +0900

    Update 4 エクセル.py

--- pages/4 エクセル.py
+++ pages/4 エクセル.py
@@ -8,33 +8,33 @@ import subprocess
 '''
 # st.title('L-gate')
 btn0 = st.button('タイピング')
-df0 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 0)
+df_0 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 0)
 btn1 = st.button('プログラミング')
-df1 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 1)
+df_1 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 1)
 btn2 = st.button('学習サイト')
-df2 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 2)
+df_2 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 2)
 btn3 = st.button('情報モラル')
-df3 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 3)
+df_3 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 3)
 btn4 = st.button('マウス・タッチ練習')
-df4 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 4)
+df_4 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 4)
 btn5 = st.button('文科省サイト')
-df5 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 5)
+df_5 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 5)
 btn6 = st.button('消去')
 
 #ボタンが押されたら処理を実行する
 if btn0:
     #wb=openpyxl.load_workbook('./data/L-gate.xlsx',data_only=True,sheet_name= 0)
-    st.dataframe(df0)
+    st.dataframe(df_0)
 elif btn1:
-    st.dataframe(df1)
+    st.dataframe(df_1)
 elif btn2:   
-    st.dataframe(df2)
+    st.dataframe(df_2)
 elif btn3:    
-    st.dataframe(df3)
+    st.dataframe(df_3)
 elif btn4:   
-    st.dataframe(df4)
+    st.dataframe(df_4)
 elif btn5:    
-    st.dataframe(df5)
+    st.dataframe(df_5)
     #st.table(df)
       
     
