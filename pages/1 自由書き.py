627f2413b034809039db6736a3776f744b7dd793
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-01-31 11:18:15 +0900

    Update 1 自由書き.py

--- pages/1 自由書き.py
+++ pages/1 自由書き.py
@@ -24,18 +24,17 @@ st.code(code,language='python')
 ## 唐津市の地図
 ## マップをプロット
 '''
-import pandas as pd
-import numpy as np
+#import pandas as pd
+#import numpy as np
 # lat/緯度、lon/経度…唐津市(E129°58'13" N33°37'48.72")
 # 100行の２列
-df=pd.DataFrame(
-    np.random.rand(100,2)/[50,50]+[33.43,130.05],
-    columns=['lat','lon']
- )
-st.map(df)
+#df=pd.DataFrame(
+#    np.random.rand(100,2)/[50,50]+[33.43,130.05],
+#    columns=['lat','lon']
+# )
+#st.map(df)
 
 # テキストボックスをサイドバーに
 #st.sidebar.write('あなたについて答えて下さい')
 #st.sidebar.text_input('名前は？')
 #st.sidebar.text_input('住所は？')
-
