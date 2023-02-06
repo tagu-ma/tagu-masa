39edeb0e8c5f1c63cd069883510b1877d80e650e
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-02-06 22:09:22 +0900

    Update 4 エクセル.py

--- pages/4 エクセル.py
+++ pages/4 エクセル.py
@@ -9,7 +9,7 @@ import subprocess
 
 # st.title('L-gate')
 btn1 = st.button('タイピング')
-df_1 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 0)
+#df_1 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 0)
 btn2 = st.button('プログラミング')
 df_2 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 2)
 #wb_1=openpyxl.load_workbook('./data/L-gate.xlsx',data_only=True,sheet_name= 2)
@@ -25,6 +25,7 @@ btn7 = st.button('消去')
 
 #ボタンが押されたら処理を実行する
 if btn1:
+    df_1 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 0)
     st.dataframe(df_1)
 elif btn2:
     st.dataframe(df_2)
