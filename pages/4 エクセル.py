38cdc3c9c71c55222da0c5ce657e998dcfc05a08
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-02-06 21:52:06 +0900

    Update 4 エクセル.py

--- pages/4 エクセル.py
+++ pages/4 エクセル.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import subprocess
+import openpyxl
 
 # Excelファイルの読込➀
 '''
@@ -17,36 +18,7 @@ if uploaded_file is not None:
 else:
     st.write("Please upload an Excel file.")
 
-
-df_0 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 0)
-btn1 = st.button('プログラミング')
-df_1 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 1)
-btn2 = st.button('学習サイト')
-df_2 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 2)
-btn3 = st.button('情報モラル')
-df_3 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 3)
-btn4 = st.button('マウス・タッチ練習')
-df_4 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 4)
-btn5 = st.button('文科省サイト')
-df_5 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 5)
-btn6 = st.button('消去')
-
-#ボタンが押されたら処理を実行する
-if btn0:
-    #wb=openpyxl.load_workbook('./data/L-gate.xlsx',data_only=True,sheet_name= 0)
-    st.dataframe(df_0)
-elif btn1:
-    st.dataframe(df_1)
-elif btn2:   
-    st.dataframe(df_2)
-elif btn3:    
-    st.dataframe(df_3)
-elif btn4:   
-    st.dataframe(df_4)
-elif btn5:    
-    st.dataframe(df_5)
-    #st.table(df)
-      
+     
     
 # マクロの起動➁
 '''
