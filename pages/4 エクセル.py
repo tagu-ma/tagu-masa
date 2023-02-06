b9ef9660d345c2e1af12d6b7417cdf08a56deb24
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-02-06 21:49:08 +0900

    Update 4 エクセル.py

--- pages/4 エクセル.py
+++ pages/4 エクセル.py
@@ -6,6 +6,18 @@ import subprocess
 '''
 ## L-gate おおすすめサイト
 '''
+
+# ファイルアップロードウィジェットを作成
+uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx"])
+
+if uploaded_file is not None:
+    # ファイルアップロードウィジェットから読み込んだExcelファイルを読み込む
+    wb = openpyxl.load_workbook(uploaded_file, data_only=True)
+    st.write("Excel file uploaded and loaded successfully!")
+else:
+    st.write("Please upload an Excel file.")
+
+
 df_0 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 0)
 btn1 = st.button('プログラミング')
 df_1 = pd.read_excel('./data/L-gate.xlsx',sheet_name= 1)
