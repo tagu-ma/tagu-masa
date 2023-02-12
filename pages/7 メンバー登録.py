33551a8364347b869b2787fb61bf520bb2e6ed1f
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-02-12 14:55:58 +0900

    Update 7 メンバー登録.py

--- pages/7 メンバー登録.py
+++ pages/7 メンバー登録.py
@@ -13,7 +13,7 @@ with st.form("my_form"):
     cancelled = st.form_submit_button('キャンセル')
     
     if submitted:
-        st.write("名前は:",name)
+        st.write("名前:",name)
     if cancelled:
         st.write("キャンセルされました")
 
