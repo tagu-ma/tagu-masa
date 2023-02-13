f532c93dc6f0d558ba965cd4a5ea50b80bc7ed42
tagu-ma <123178356+tagu-ma@users.noreply.github.com>
2023-02-13 14:30:17 +0900

    Update 7 メンバー登録.py

--- pages/7 メンバー登録.py
+++ pages/7 メンバー登録.py
@@ -19,6 +19,6 @@ with st.form("my_form"):
 
 # メンバー一覧
 btn=st.button('ファンメンバー一覧')
-#df =pd.read_excel('./data/menber.xlsx')
-#if btn:
-#    st.dataframe(df)
+df =pd.read_excel('./data/menber.csv')
+if btn:
+    st.dataframe(df)
